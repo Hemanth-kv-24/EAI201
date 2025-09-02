642ffddc22f8a45770e323e52a61f84ce912fbd6
Hemanth kumar K V <hemanthkv.eai24@chanakyauniversity.edu.in>
2025-09-02 10:53:54 +0530

    Create StudentGradig.py

---
+++ StudentGradig.py
@@ -0,0 +1,34 @@
+def ai_grading_assistant():
+    print("""Hello..!
+      I am your Grading AI Assistant and I will help you get your grades based on your marks.
+      Please Enter the options below..""")
+    
+    name=input("Enter student's name:")
+
+    subs=int(input("How many subjects?:"))
+    dict={}
+    
+    for i in range(0,subs):
+        subject=input(f"Enter name of subject {i+1}: ")
+        marks=float(input(f"Enter marks for {subject} (out of 100): "))
+        dict[subject]=marks
+
+    print("Final Grading for:",name)
+    total=0
+    for subject, marks in dict.items():
+        total=total+marks
+
+    avg=total/subs
+
+    print(f"Average Marks: {avg:.2f}")
+
+    if avg>=90:
+        print("Final Grade: A")
+    elif avg>=80:
+        print("Final Grade: B")
+    elif avg>=70:
+        print("Final Grade: C")
+    elif avg>=60:
+        print("Final Grade: D")
+    else:
+        print("Final Grade: W")
